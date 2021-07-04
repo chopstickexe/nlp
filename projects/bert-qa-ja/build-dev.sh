1381461a6dad1a02b8951ceefbc467936f453b95
Risa Nishiyama <lisa.nishiyama@gmail.com>
2021-07-04 13:40:00 +0900

    [bert-qa-ja] Set workdir of the container

--- projects/bert-qa-ja/build-dev.sh
+++ projects/bert-qa-ja/build-dev.sh
@@ -4,7 +4,9 @@ DEV_IMAGE=bert-qa-ja-dev
 DEV_CONTAINER=bert-qa-ja-dev
 
 docker build --target dev -f Dockerfile -t ${DEV_IMAGE} .
-docker run -itd --gpus all --name="${DEV_CONTAINER}" -v `pwd`:/work \
+docker run -itd --gpus all --name="${DEV_CONTAINER}" \
+  -v `pwd`:/work \
+  -w /work \
   -e LOCAL_UID=$(id -u $USER) \
   -e LOCAL_GID=$(id -g $USER) \
   -e LOCAL_USER=$USER \
